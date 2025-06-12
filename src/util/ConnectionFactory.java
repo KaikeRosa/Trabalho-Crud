ee78d8c2e37e8f523e11406d857463541e532c5d
KaikeRosa <twok3345@gmail.com>
2025-06-12 18:43:59 -0300

    Update ConnectionFactory.java

--- src/util/ConnectionFactory.java
+++ src/util/ConnectionFactory.java
@@ -4,10 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//altere conforme os seus dados
 public class ConnectionFactory {
-    private static final String URL = "jdbc:mysql://localhost:3306/cadastro_escolar";
-    private static final String USER = "root";
-    private static final String PASSWORD = "3345261789";  //alterar conforme a senha
+    private static final String url = "jdbc:mysql://localhost:3306/cadastro_escolar";
+    private static final String user = "seu_usuario";
+    private static final String password = "sua_senha";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
